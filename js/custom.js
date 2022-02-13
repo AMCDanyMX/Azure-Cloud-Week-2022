11482bdf09cbe6b1c358e36168b446f70bd0a9b7
Daniel Castillo <60952744+AMCDanyMX@users.noreply.github.com>
2022-02-12 19:53:24 -0600

    ACW 2022
    Webpage files Azure Cloud Week February 2022

---
+++ js/custom.js
@@ -0,0 +1,114 @@
+
+ /* jQuery Pre loader
+  -----------------------------------------------*/
+
+  $(document).on("ready",function(){
+    $(window).on('load', function(){
+        $('#pageloader-overlay').fadeOut(1500);
+    });
+  });
+
+/* Mobile Navigation
+    -----------------------------------------------*/
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+
+/* HTML document is loaded. DOM is ready. 
+-------------------------------------------*/
+$(document).ready(function() {
+
+  /* Hide mobile menu after clicking on a link
+    -----------------------------------------------*/
+    $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+
+
+ /* Parallax section
+    -----------------------------------------------*/
+  function initParallax() {
+    $('#intro').parallax("100%", 0.9);
+    $('#informacion').parallax("100%", 0.9);
+    $('#detail').parallax("100%", 0.9);
+    $('#video').parallax("100%", 0.3);
+    $('#speakers').parallax("100%", 0.1);
+    $('#program').parallax("100%", 0.2);
+    $('#register').parallax("100%", 0.1);
+    $('#faq').parallax("100%", 0.3);
+    $('#venue').parallax("100%", 0.1);
+    $('#sponsors').parallax("100%", 0.3);
+    $('#equipo').parallax("100%", 9.0);
+
+  }
+  initParallax();
+
+
+  /* Owl Carousel
+  -----------------------------------------------*/
+  $(document).ready(function() {
+    $("#owl-speakers").owlCarousel({
+      autoPlay: 3000,
+      items : 4,
+      itemsDesktop : [1199,2],
+      itemsDesktopSmall : [979,1],
+      itemsTablet: [768,1],
+      itemsTabletSmall: [985,2],
+      itemsMobile : [479,1],
+    });
+  });
+
+
+  /* Back top
+  -----------------------------------------------*/
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 200) {
+        $('.go-top').fadeIn(200);
+        } else {
+          $('.go-top').fadeOut(200);
+        }
+        });   
+        // Animate the scroll to top
+      $('.go-top').click(function(event) {
+        event.preventDefault();
+      $('html, body').animate({scrollTop: 0}, 300);
+      })
+
+
+  /* wow
+  -------------------------------*/
+  new WOW({ mobile: false }).init();
+
+  });
+
+
+  const body = document.body;
+const nav = document.querySelector(".nav");
+const menu = document.querySelector(".nav .navbar-header");
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll == 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+  
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+  } else if (currentScroll < lastScroll && body.classList.contains(scrollDown)) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+  }
+  lastScroll = currentScroll;
+});
